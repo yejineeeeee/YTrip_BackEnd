98d56f91d33373637e219fa563e0c2898a4fca4f
WooJJam <jaemin5548@naver.com>
2024-05-21 17:44:26 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/CodeIt/Ytrip/YtripApplication.java
@@ -0,0 +1,13 @@
+package CodeIt.Ytrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtripApplication.class, args);
+	}
+
+}
