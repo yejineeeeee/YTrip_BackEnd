c28eaab9dee5a6fd510432d7d3a6eeb5e2e0e909
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-28 16:41:33 +0900

    :sparkles: BaseEntity 생성 (#22)

--- src/main/java/CodeIt/Ytrip/YtripApplication.java
+++ src/main/java/CodeIt/Ytrip/YtripApplication.java
@@ -2,7 +2,9 @@ package CodeIt.Ytrip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class YtripApplication {
 
--- src/main/java/CodeIt/Ytrip/user/domain/User.java
+++ src/main/java/CodeIt/Ytrip/user/domain/User.java
@@ -1,15 +1,15 @@
 package CodeIt.Ytrip.user.domain;
 
+import CodeIt.Ytrip.common.domain.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.Builder;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class User {
+public class User extends BaseEntity {
 
     @Id @GeneratedValue
     @Column(name = "user_id")
