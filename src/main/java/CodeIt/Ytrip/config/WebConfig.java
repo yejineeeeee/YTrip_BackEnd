d28fca80846d4466d0186c77d7a4cec83c268387
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-29 18:57:59 +0900

    🐛 WebConfig를 통한 CORS 문제 해결 (#38)
    * 🐛: @CrossOrigin 어노테이션을 설정하여 CORS 문제 임시 해결
    
    * 🐛: @CrossOrigin 제거
    
    * 🐛: WebConfig 설정 파일을 이용하여 CORS 문제 해결
    
    * 🐛: AuthController @CrossOrigin 삭제

--- src/main/java/CodeIt/Ytrip/config/WebConfig.java
+++ src/main/java/CodeIt/Ytrip/config/WebConfig.java
@@ -3,6 +3,7 @@ package CodeIt.Ytrip.config;
 import CodeIt.Ytrip.common.interceptor.JwtInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,5 +23,21 @@ public class WebConfig implements WebMvcConfigurer {
 //                .addPathPatterns("/**")
 //                .excludePathPatterns("/api/auth/**","/test/**");
 //    }
+
+    /**
+     * CORS 설정
+     * 현재 모든 리소스의 요청을 허용
+     * 추후 프론트 서버만 요청 허용하도록 변경 필요
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+//                .allowCredentials(true)
+                .maxAge(3600);
+    }
 }
 
