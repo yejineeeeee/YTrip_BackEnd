5cb39a96b82262551cf78dde89b19d9e736bc56d
재민 <111514410+WooJJam@users.noreply.github.com>
2024-05-29 18:10:10 +0900

    🐛: @CrossOrigin 어노테이션을 설정하여 CORS 문제 임시 해결 (#37)

--- src/main/java/CodeIt/Ytrip/auth/controller/AuthController.java
+++ src/main/java/CodeIt/Ytrip/auth/controller/AuthController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
 @RequiredArgsConstructor
 public class AuthController {
 
--- src/main/java/CodeIt/Ytrip/video/controller/VideoController.java
+++ src/main/java/CodeIt/Ytrip/video/controller/VideoController.java
@@ -5,16 +5,14 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/video")
 public class VideoController {
 
