436c41f480f98bd61776495322346398db563573
WooJJam <jaemin5548@naver.com>
2024-05-29 15:08:49 +0900

    :white_check_mark: 테스트 코드 수정

--- src/test/java/CodeIt/Ytrip/video/service/VideoServiceTest.java
+++ src/test/java/CodeIt/Ytrip/video/service/VideoServiceTest.java
@@ -3,11 +3,14 @@ package CodeIt.Ytrip.video.service;
 import CodeIt.Ytrip.video.domain.Video;
 import CodeIt.Ytrip.video.repository.VideoRepository;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.LockModeType;
 import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -25,11 +28,21 @@ class VideoServiceTest {
 
     @Test
     public void 비디오_목록_조회() throws Exception {
-        //given
+
+        for (long i = 1L; i < 13L; i++) {
+            System.out.println("i = " + i);
+            Video test = Video.builder()
+                    .likeCount(100)
+                    .title("Test Title" + i)
+                    .url("Test Url"+i)
+                    .tag("Test Tag"+i)
+                    .content("Test Content"+i)
+                    .build();
+            videoRepository.save(test);
+        }
+
         List<Video> findVidoes = videoRepository.findTop12ByOrderByLikeCountDesc();
-        //when
         int size = findVidoes.size();
-        //then
         Assertions.assertThat(size).isEqualTo(12);
     }
 
