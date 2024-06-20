1345a019b81ffa6e33a11f4e6a8128ac7116f21d
WooJJam <jaemin5548@naver.com>
2024-06-21 01:21:59 +0900

    :sparkles: Index 매핑 생성

--- src/main/java/CodeIt/Ytrip/admin/AdminController.java
+++ src/main/java/CodeIt/Ytrip/admin/AdminController.java
@@ -5,20 +5,21 @@ import CodeIt.Ytrip.common.reponse.SuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
 
-    @PostMapping("/video")
+    @GetMapping
+    public ResponseEntity<?> ok() {
+        return ResponseEntity.ok(null);
+    }
+
+    @PostMapping("/admin/video")
     public ResponseEntity<SuccessResponse<StatusCode>> saveVideoAndVideoCourse(@RequestBody SaveVideoRequest saveVideoRequest) {
         adminService.saveVideoAndVideoCourse(saveVideoRequest);
         return ResponseEntity.ok(SuccessResponse.of(StatusCode.SUCCESS.getCode(), StatusCode.SUCCESS.getMessage()));
